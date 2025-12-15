3fcd805d72a2919929a157bc685d7656a0b9b88d
xnil6xdev <xnil6x@gmail.com>
2025-12-15 14:35:45 +0000

    Hu

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const PORT = process.env.PORT || 3001;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>Hello from my-app!</h1><p>Your project is running successfully.</p>');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
